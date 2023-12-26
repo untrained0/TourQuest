57c95b641d7b490d1c3ff372a9fb220a7698a251
Soham <sohamkelaskar@gmail.com>
2023-12-26 23:44:41 +0530

    stored the recorded video in firebase storage.

--- app/Components/AddPost/Recorder.js
+++ app/Components/AddPost/Recorder.js
@@ -1,22 +1,25 @@
-import { View, Text, TouchableOpacity, StyleSheet, type } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, type, Alert } from 'react-native'
 import React, { useEffect, useRef, useState } from 'react'
 import { Camera } from 'expo-camera';
 import { Audio } from 'expo-av';
 import { Ionicons } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Recorder() {
 
+    const navigation = useNavigation();
     const cameraRef = useRef(null);
 
+    const [type, setType] = useState(Camera.Constants.Type.back);
     const [values, setValues] = useState({
         hasPermission: [],
-        cameraType: Camera.Constants.Type.back,
+        // cameraType: Camera.Constants.Type.back,
         isFlashLightOn: Camera.Constants.FlashMode.off,
         videoStatus: 0
     });
 
-    const { hasPermission, cameraType, isFlashLightOn, videoStatus } = values;
+    const { hasPermission, isFlashLightOn, videoStatus } = values;
 
     useEffect(() => {
         getPermissions();
@@ -28,79 +31,105 @@ export default function Recorder() {
         setValues({ ...values, hasPermission: [status, AudioStatus.status] });
     }
 
+    if (hasPermission === null) {
+        return <View />;
+    }
+
+    if (!hasPermission) {
+        Alert.alert(
+            'Camera Permission Required',
+            'Please grant camera permission to use this feature.',
+            [{ text: 'OK' }]
+        );
+        return null;
+    }
+
+    
+    const toggleCameraType = () => {
+        setType(current =>
+          current === Camera.Constants.Type.back
+            ? Camera.Constants.Type.front
+            : Camera.Constants.Type.back
+        );
+      };
+
     const videoRecord = async () => {
         if (!videoStatus && cameraRef.current) {
-            
-            setValues({
-                ...values,
-                videoStatus: 1,
-                isFlashLightOn: isFlashLightOn ?
-                    Camera.Constants.FlashMode.torch :
-                    isFlashLightOn
-            });
-            
-            await cameraRef.current
-                .recordAsync()
-                .then((data) => {
-                    console.log(data);
-                })
-                .catch((e) => console.log(e));
-
+          setValues({
+            ...values,
+            videoStatus: 1,
+            isFlashLightOn: isFlashLightOn
+              ? Camera.Constants.FlashMode.torch
+              : isFlashLightOn,
+          });
+    
+          await cameraRef.current
+            .recordAsync()
+            .then((data) => {
+              // console.log(data);
+              navigation.navigate('AddPost', { video: data.uri });
+            })
+            .catch((e) => console.log(e));
         } else {
-            try {
-                await cameraRef.current.stopRecording();
-                setValues({
-                    ...values,
-                    videoStatus: 0,
-                    isFlashLightOn: Camera.Constants.FlashMode.off,
-                })
-            } catch (error) {
-                console.log(error);
-            }
+          try {
+            await cameraRef.current.stopRecording();
+            setValues({
+              ...values,
+              videoStatus: 0,
+              isFlashLightOn: Camera.Constants.FlashMode.off,
+            });
+          } catch (error) {
+            console.log(error);
+          }
         }
-    }
+      };
+
+    // if (hasPermission.length === 0) {
+    //     return <View/>
+    // }
+
 
     return (
         <View style={styles.container}>
-            <Camera ref={cameraRef} flashMode={isFlashLightOn} style={styles.camera} type={type}>
-                <View style={styles.buttonContainer}>
-                    <TouchableOpacity style={styles.button}
-                        onPress={() => {
-                            if (!videoStatus) {
-                                setValues({
-                                    ...values,
-                                    isFlashLightOn: isFlashLightOn ?
-                                        Camera.Constants.FlashMode.off :
-                                        Camera.Constants.FlashMode.on
-                                })
-                            }
-                        }}>
-                        <MaterialCommunityIcons name={isFlashLightOn ? 'flashlight-off' : 'flashlight'} size={24} color="black" />
-                    </TouchableOpacity>
-
-                    <TouchableOpacity style={styles.button}
-                        onPress={() => videoRecord()}>
-                        <MaterialCommunityIcons name={videoStatus ? "stop" : 'record'} size={24} color="black" />
-                    </TouchableOpacity>
-                    
-                    <TouchableOpacity style={styles.button}
-                    onPress={() => {
-                        if (!videoStatus) {
-                            setValues({
-                                ...values,
-                                cameraType: cameraType ?
-                                Camera.Constants.Type.back:
-                                Camera.Constants.Type.front
-                            })
-                        }
-                      }} >
-                        <MaterialCommunityIcons name="camera-front" size={24} color="black" />
-                    </TouchableOpacity>
-                </View>
-            </Camera>
+          <Camera ref={cameraRef} flashMode={isFlashLightOn} style={styles.camera} type={type}>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                  if (!videoStatus) {
+                    setValues({
+                      ...values,
+                      isFlashLightOn: isFlashLightOn
+                        ? Camera.Constants.FlashMode.off
+                        : Camera.Constants.FlashMode.on,
+                    });
+                  }
+                }}
+              >
+                <MaterialCommunityIcons
+                  name={isFlashLightOn ? 'flashlight' : 'flashlight-off'}
+                  size={24}
+                  color="black"
+                />
+              </TouchableOpacity>
+    
+              <TouchableOpacity style={styles.button} onPress={videoRecord}>
+                <MaterialCommunityIcons
+                  name={videoStatus ? 'stop' : 'record'}
+                  size={24}
+                  color="black"
+                />
+              </TouchableOpacity>
+    
+              <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+                <MaterialCommunityIcons name="camera-front" size={24} color="black" />
+              </TouchableOpacity>
+            </View>
+          </Camera>
         </View>
-    )
-}
+      );
+    };
+
 
 const styles = StyleSheet.create({
     container: {
--- app/Screens/AddPostScreen.js
+++ app/Screens/AddPostScreen.js
@@ -1,35 +1,97 @@
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
-import React from 'react'
-import Colors from '../Utils/Colors'
-import { useNavigation } from '@react-navigation/native';
+import { View, Text, TouchableOpacity, StyleSheet, Button } from 'react-native';
+import React from 'react';
+import Colors from '../Utils/Colors';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { Video} from 'expo-av';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { app } from '../../firebaseConfig';
 
-export default function AddPostScreen(props) {
 
+export default function AddPostScreen() {
+
+  const storage = getStorage(app);
+  // const db = getFirestore(app);
+
+  const video = React.useRef(null);
   const navigation = useNavigation();
+  const params = useRoute().params;
 
-const onRecord = () => {
-  // console.log(props);
-  navigation.navigate('Recorder');
-}
+  const onRecord = () => {
+    navigation.navigate('Recorder');
+  };
+
+  const onAddPost = async (fileUri) => {
+    const metadata = {
+      contentType: 'video/mp4', // Set the appropriate content type for your videos
+    };
+  
+    const response = await fetch(fileUri);
+    const blob = await response.blob();
+  
+    const storageRef = ref(storage, 'post-videos/' + Date.now() + '.mp4');
+    const uploadTask = uploadBytesResumable(storageRef, blob, metadata);
+  
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        // Handle upload progress if needed
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+      },
+      (error) => {
+        console.error('Error uploading video:', error);
+      },
+      async () => {
+        // Handle successful upload completion
+        const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+        console.log('File available at:', downloadURL);
+        // You can save the downloadURL in your database or perform any other necessary actions here
+      }
+    );
+  };
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={() => onRecord()}>
-        <Text style={{color: Colors.WHITE, fontSize: 20, padding: 5}}>Record</Text>
-      </TouchableOpacity>
+      {params?.video ? (
+        <Video
+          ref={video}
+          style={{ width: '100%', flex: 1, marginBottom: 5 }}
+          source={{
+            uri: params?.video,
+          }}
+          useNativeControls
+          resizeMode="cover"
+          isLooping
+        />
+      ) : (
+        <TouchableOpacity style={styles.recordButton} onPress={() => onRecord()}>
+          <Text style={{ color: Colors.WHITE, fontSize: 20, padding: 5 }}>Record</Text>
+        </TouchableOpacity>
+      )}
+
+      {params?.video && (
+        <TouchableOpacity style={styles.addPostButton} onPress={() => onAddPost(params?.video)}>
+          <Text style={{ color: Colors.WHITE, fontSize: 20, padding: 5 }}>Add Post</Text>
+        </TouchableOpacity>
+      )}
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
-  button: {
+  recordButton: {
     backgroundColor: Colors.GREEN,
     padding: 12,
     borderRadius: 15,
-  }
-})
+    marginBottom: 10,
+  },
+  addPostButton: {
+    backgroundColor: Colors.BLUE, // Use a color of your choice
+    padding: 12,
+    borderRadius: 15,
+  },
+});
